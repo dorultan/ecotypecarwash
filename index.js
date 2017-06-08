ecb721e7d55ec627c806f0e9987f9948df8da597
dorultan <dorultanianosgyorgy@gmail.com>
2017-06-08 14:29:52 +0100

    change

--- index.js
+++ index.js
@@ -2,6 +2,33 @@
 var express = require('express');
 //call express
 var app = express();
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+	host: 'ecotypecarwash.herokuapp.com',
+	port: 465,
+	secure: true,
+	auth: {
+		user: 'robert@ecotypecarwash.com',
+		pass: 'robertloza93'
+	}
+})
+
+
+let mailOptions = {
+	from: '"dorultan <dorultanianosgyorgy.com"',
+	to: 'robert@ecotypecarwash.com',
+	subject: 'hello world ? ',
+	html: '<b>Hello cool world</b>'
+}
+
+transporter.sendMail(mailOptions, (error, info) => {
+	if(error) {
+		return console.log(error);
+	}
+
+	console.log('Message is sent successful', info.messageId, info.response);
+})
 
 app.set('port', process.env.PORT || 5000);
 
